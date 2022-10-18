78ff9ba5f43cddffab43043498511bcc326d4a1d
Tushar-Jha <jhatushar18012001@gmail.com>
2022-10-18 17:37:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
 function App() {
+  const [countries,setCountries]=React.useState([]);
+
+  const [weather,setWeather]=React.useState({});
+
+  const getCountries = async () =>{
+    const res= await fetch("https://countriesnow.space/api/v0.1/countries");
+    const data =await res.json();
+    setCountries(data.data);
+  }
+
+  React.useEffect(()=>{
+    getCountries();
+  },[])
+
+  const handleClick = async(city) =>{
+    const apiKey="6ac8ceb444c7b2245255cdd69c56c70a";
+    const url="https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+apiKey
+    const weather=await fetch(url);
+    const data=await weather.json();
+    console.log(data.main)
+    setWeather(data.main)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='x'>
+      <h1>Countries</h1>
+      <ul>
+        {countries.map((c)=>{
+          return (<li className='list-item' >
+            <div onClick={()=>{handleClick(c.cities[0]);}}>{c.country}</div>
+          </li>)
+        })}
+      </ul>
+      </div>
+      <div className='x'>
+        <h3>Temperature: {weather.temp}</h3>
+        <h3>Pressure: {weather.pressure}</h3>
+        <h3>Humidity: {weather.humidity}</h3>
+      </div>
+      
     </div>
   );
 }
